1699d36d1c6a4338cdadcddac4afc2ac3e0a2106
Maksym Storozhenko <mstorozhenko@apple.com>
2025-02-19 19:54:26 +0000

    fix session clear and initialization

--- network/data_fetch/parallel_stock_data_fetcher.py
+++ network/data_fetch/parallel_stock_data_fetcher.py
@@ -18,18 +18,9 @@ class ParallelStockDataFetcher:
         self.max_workers = max_workers
 
     @staticmethod
-    def fetch_data_for_ticker(ticker, period):
+    def fetch_data_for_ticker(ticker, period, cached_session):
         try:
-            # session = requests_cache.CachedSession('yfinance.cache')
-            session = CachedLimiterSession(
-                limiter=Limiter(RequestRate(2, Duration.SECOND)),
-                bucket_class=MemoryQueueBucket,
-                backend=SQLiteCache("yfinance.cache"),
-                expire_after=36000 # 10h
-            )
-            session.cache.delete(expired=True)
-            session.headers['User-agent'] = 'scanX/1.0'
-            stock = yf.Ticker(ticker, session=session)
+            stock = yf.Ticker(ticker, session=cached_session)
             stock_history = stock.history(period=period).iloc[::-1]
             stock_info = stock.info
             adr = Adr.calculate_adr(stock_history)
@@ -39,7 +30,17 @@ class ParallelStockDataFetcher:
             return ticker, None, None, None
 
     def fetch_data(self, tickers, period):
+        # session = requests_cache.CachedSession('yfinance.cache')
+        cached_session = CachedLimiterSession(
+            limiter=Limiter(RequestRate(2, Duration.SECOND)),
+            bucket_class=MemoryQueueBucket,
+            backend=SQLiteCache("yfinance.cache"),
+            expire_after=36000  # 10h
+        )
+        cached_session.cache.delete(expired=True)
+        cached_session.headers['User-agent'] = 'scanX/1.0'
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            results = list(executor.map(lambda ticker: self.fetch_data_for_ticker(ticker, period), tickers))
+            results = list(
+                executor.map(lambda ticker: self.fetch_data_for_ticker(ticker, period, cached_session), tickers))
 
         return results
